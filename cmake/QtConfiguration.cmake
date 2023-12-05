1aae8da25def054c591500dde3eb680e4c5ad9fd
Franziska Wegner <franzi.wegner@googlemail.com>
2023-12-05 10:50:57 -0800

    Switch from QT5 to QT6 in CMake
    Changes to be committed:
    	modified:   CMakeLists.txt
    	modified:   cmake/QtConfiguration.cmake

--- cmake/QtConfiguration.cmake
+++ cmake/QtConfiguration.cmake
@@ -1,51 +1,51 @@
 # QtConfiguration.cmake
-# 
+#
 #   Created on: Jan 30, 2019
 #       Author: Franziska Wegner
-#       
-# Find QT5 include directory and libraries for linking and its required modules.
-# 
+#
+# Find QT6 include directory and libraries for linking and its required modules.
+#
 # * Once done this will define
-#   * Qt5_FOUND         - system has Qt5
-#   * QT5_LIBRARIES     - the libraries needed to use Qt5
-#   * QTCORE_LOCATION   - location of the module Qt5::Core
-#   * QTQUICK_LOCATION  - location of the module Qt5::Quick
+#   * Qt6_FOUND         - system has Qt6
+#   * QT6_LIBRARIES     - the libraries needed to use Qt6
+#   * QTCORE_LOCATION   - location of the module Qt6::Core
+#   * QTQUICK_LOCATION  - location of the module Qt6::Quick
 
 # Find includes in corresponding build directories
-set(CMAKE_INCLUDE_CURRENT_DIR ON)
+set( CMAKE_INCLUDE_CURRENT_DIR ON )
 
 # Instruct CMake to run moc automatically when needed.
-set(CMAKE_AUTOMOC ON)
+set( CMAKE_AUTOMOC ON )
 
 # https://cmake.org/cmake/help/v3.0/prop_tgt/AUTOUIC.html
-set(CMAKE_AUTOUIC ON)
-# Note that without enabling it headers such as "ui_mainwindow.h" and 
+set( CMAKE_AUTOUIC ON )
+# Note that without enabling it headers such as "ui_mainwindow.h" and
 # "ui_*.h" will not be found.
 
 ####################################################################
 # Find QT5 #########################################################
 ####################################################################
-message(STATUS "Looking for Qt5")
-FIND_PACKAGE(Qt5 COMPONENTS Core Quick Widgets Concurrent REQUIRED) # 
-get_target_property(QTCORE_LOCATION Qt5::Core LOCATION)
-get_target_property(QTQUICK_LOCATION Qt5::Quick LOCATION) # LOCATION_Release
-get_target_property(QTWIDGETS_LOCATION Qt5::Widgets LOCATION) # LOCATION_Release
-get_target_property(QTConcurrent_LOCATION Qt5::Concurrent LOCATION) # LOCATION_Release
+message(STATUS "Looking for Qt6")
+find_package(Qt6 COMPONENTS Core Quick Widgets Concurrent REQUIRED)
+get_target_property(QTCORE_LOCATION Qt6::Core LOCATION)
+get_target_property(QTQUICK_LOCATION Qt6::Quick LOCATION) # LOCATION_Release
+get_target_property(QTWIDGETS_LOCATION Qt6::Widgets LOCATION) # LOCATION_Release
+get_target_property(QTConcurrent_LOCATION Qt6::Concurrent LOCATION) # LOCATION_Release
 add_definitions( ${QT_DEFINITIONS} )
 
 ####################################################################
 # Output Message ###################################################
 ####################################################################
-if(Qt5_FOUND)
-    message( STATUS "${MY_SPACE}Found Qt5 Core module in \"${QTCORE_LOCATION}\" " )
-    message( STATUS "${MY_SPACE}Found Qt5 Quick module in \"${QTQUICK_LOCATION}\" " )
-    message( STATUS "${MY_SPACE}Found Qt5 Widgets module in \"${QTWIDGETS_LOCATION}\" " )
-    message( STATUS "${MY_SPACE}Found Qt5 Concurrent module in \"${QTConcurrent_LOCATION}\" " )
-endif()
+if( Qt6_FOUND )
+    message( STATUS "${MY_SPACE}Found Qt6 Core module in \"${QTCORE_LOCATION}\" " )
+    message( STATUS "${MY_SPACE}Found Qt6 Quick module in \"${QTQUICK_LOCATION}\" " )
+    message( STATUS "${MY_SPACE}Found Qt6 Widgets module in \"${QTWIDGETS_LOCATION}\" " )
+    message( STATUS "${MY_SPACE}Found Qt6 Concurrent module in \"${QTConcurrent_LOCATION}\" " )
+endif( Qt6_FOUND )
 
 ####################################################################
 # Check if QT is available #########################################
 ####################################################################
-set ( QT5_LIBRARIES Qt5::Core Qt5::Quick Qt5::Widgets )
+set( QT6_LIBRARIES Qt6::Core Qt6::Quick Qt6::Widgets )
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(QT5 DEFAULT_MSG QT5_LIBRARIES)
+find_package_handle_standard_args(QT6 DEFAULT_MSG QT6_LIBRARIES)
