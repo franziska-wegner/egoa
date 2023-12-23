f983065af77adbbf96d0aa118305711490b136e5
Franziska Wegner <franzi.wegner@googlemail.com>
2023-12-23 01:12:14 -0800

    Add IO tests for the IEEE CDF Matlab Parser

---
+++ tests/IO/TestIeeeCdfMatlabParser.cpp
@@ -0,0 +1,35 @@
+/*
+ * TestIeeeCdfMatlabParser.cpp
+ *
+ *  Created on: Jan 29, 2019
+ *      Author: Franziska Wegner
+ */
+
+#include "TestIeeeCdfMatlabParser.hpp"
+#include "IO/IeeeCdfMatlabParser.hpp"
+
+namespace egoa {
+namespace test {
+
+TestIeeeCdfMatlabParser::TestIeeeCdfMatlabParser () {}
+
+TestIeeeCdfMatlabParser::~TestIeeeCdfMatlabParser () {}
+
+void TestIeeeCdfMatlabParser::SetUp () {}
+
+void TestIeeeCdfMatlabParser::TearDown () {}
+
+TEST_F ( TestIeeeCdfMatlabParser, ReadIeeeCdfMatlab ) {
+    // TODO
+}
+
+TEST_F ( TestIeeeCdfMatlabParser, WriteIeeeCdfMatlab ) {
+    // TODO
+}
+
+TEST_F ( TestIeeeCdfMatlabParser, DeathTest ) {
+    // TODO
+}
+
+} // namespace test
+} // namespace egoa
