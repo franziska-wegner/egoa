b8d805d3fa8fb8459f277accda8a815c406f07a1
Franziska Wegner <franzi.wegner@googlemail.com>
2023-12-30 10:06:14 -0800

    DeathTest condition for `RealPowerLoadAt` with exception
    Changes to be committed:
    	modified:   tests/DataStructures/Graphs/TestPowerGrid.cpp

--- tests/DataStructures/Graphs/TestPowerGrid.cpp
+++ tests/DataStructures/Graphs/TestPowerGrid.cpp
@@ -4431,6 +4431,7 @@ TEST_F  ( TestPowerGridAcm2018MtsfFigure4b
 // ***********************************************************************
 // ***********************************************************************
 
+#ifdef EGOA_ENABLE_ASSERTION
 TEST_F  ( TestNetworkEmptyDeathTest
         , RealPowerLoadAt )
 { // Total real power load for a snapshot (here 0 implicitly) for a vertexId
@@ -4444,6 +4445,39 @@ TEST_F  ( TestNetworkEmptyDeathTest
     ASSERT_DEATH ( {networkConst_.RealPowerLoadAt ( static_cast<Types::vertexId>( 0 ) );}
                  , assertionString );
 }
+#else
+#ifdef EGOA_ENABLE_EXCEPTION_HANDLING
+TEST_F  ( TestNetworkEmpty
+        , RealPowerLoadAtExceptionHandling )
+{ // Total real power load for a snapshot (here 0 implicitly) for a vertexId
+    auto assertionString = buildAssertionString ( "PowerGrid.hpp"
+                                                , "PowerGrid"
+                                                , "RealPowerLoadAt"
+                                                , "Graph\\(\\).VertexExists\\( vertexId \\)");
+
+    try {
+        network_.RealPowerLoadAt ( static_cast<Types::vertexId>( 0 ) );
+    } catch ( std::runtime_error const & error )
+    {
+        EXPECT_THAT ( error.what(), MatchesRegex(assertionString.c_str()) );
+    } catch ( ... )
+    {
+        FAIL()  << "Expected std::runtime_error with message: "
+                << assertionString;
+    }
+    try {
+        networkConst_.RealPowerLoadAt ( static_cast<Types::vertexId>( 0 ) );
+    } catch ( std::runtime_error const & error )
+    {
+        EXPECT_THAT ( error.what(), MatchesRegex(assertionString.c_str()) );
+    } catch ( ... )
+    {
+        FAIL()  << "Expected std::runtime_error with message: "
+                << assertionString;
+    }
+}
+#endif // ifdef EGOA_ENABLE_EXCEPTION_HANDLING
+#endif // ifdef EGOA_ENABLE_ASSERTION
 
 TEST_F  ( TestPowerGridAcm2018MtsfFigure4a
         , RealPowerLoadAt )
