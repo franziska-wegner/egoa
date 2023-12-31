f9c17f7c7d5abe488dcca52c453a09b424d6d1c4
Franziska Wegner <franzi.wegner@googlemail.com>
2023-12-31 01:40:32 -0800

    DeathTest condition for `TimestampAt` with exception
    Changes to be committed:
    	modified:   tests/DataStructures/Graphs/TestPowerGrid.cpp

--- tests/DataStructures/Graphs/TestPowerGrid.cpp
+++ tests/DataStructures/Graphs/TestPowerGrid.cpp
@@ -5207,6 +5207,7 @@ TEST_F ( TestPowerGridPyPsaExample
 // ***********************************************************************
 // ***********************************************************************
 
+#ifdef EGOA_ENABLE_ASSERTION
 TEST_F ( TestNetworkEmpty
        , TimestampAt )
 {
@@ -5217,6 +5218,28 @@ TEST_F ( TestNetworkEmpty
     ASSERT_DEATH ( {network_.TimestampAt ( 0 );}
                  , assertionString );
 }
+#else
+#ifdef EGOA_ENABLE_EXCEPTION_HANDLING
+TEST_F ( TestNetworkEmpty
+       , TimestampAtExceptionHandling )
+{
+    auto assertionString = buildAssertionString ( "PowerGrid.hpp"
+                                                , "PowerGrid"
+                                                , "TimestampAt"
+                                                , "timestampPosition < timestamps_.size\\(\\)");
+    try {
+        network_.TimestampAt ( 0 );
+    } catch ( std::runtime_error const & error )
+    {
+        EXPECT_THAT ( error.what(), MatchesRegex(assertionString.c_str()) );
+    } catch ( ... )
+    {
+        FAIL()  << "Expected std::runtime_error with message: "
+                << assertionString;
+    }
+}
+#endif // ifdef EGOA_ENABLE_EXCEPTION_HANDLING
+#endif // ifdef EGOA_ENABLE_ASSERTION
 
 TEST_F ( TestPowerGridAcm2018MtsfFigure4a
        , TimestampAt )
